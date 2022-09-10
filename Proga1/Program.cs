aeed9fc3d4f388513abe374a04e738aa7411803e
Andrey Makarchuk <medved886@gmail.com>
2022-09-10 15:08:33 +0300

    ДЗ №6

---
+++ Proga1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите колличество чисел для ввода: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        res[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return res;
+}
+int[] array = GetArray(size);
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        sum++;
+}
+Console.Write(String.Join(", ", array));
+Console.WriteLine($" -> {sum} больше нуля");
---
+++ Proga2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите коэфициэнты к1, b1, k2, b2: ");
+
+double[] GetArray(double size)
+{
+    double[] res = new double[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        Console.Write($"Введите коэфициэнт: ");
+        res[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return res;
+}
+double[] array = GetArray(4);
+
+double x = 0;
+double y = 0;
+
+if (array[0] == array[2])
+
+    Console.WriteLine($"Прямые (y = {array[0]} * x + {array[1]}); (y = {array[2]} * x + {array[3]}) -> не пересекаются");
+
+else
+{
+    x = (array[3] - array[1]) / (array[0] - array[2]);
+    y = array[2] * x + array[3];
+
+    Console.Write($" k1={array[0]}, b1={array[1]}, k2={array[2]}, b2={array[3]}");
+    Console.WriteLine($" -> А({x}; {y})");
+}
